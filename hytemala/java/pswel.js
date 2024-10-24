48de214d4134335ceebd6758f9b54ff509d64f7d
sanguirIS <pahimna294klenn@gmail.com>
2024-10-24 02:19:57 -0700

    commit

--- hytemala/java/pswel.js
+++ hytemala/java/pswel.js
@@ -11,17 +11,13 @@ const symbols = "!@#$%^&*()_-+=<>?/:;{}[]";
 let password = "";
 
 function getRandomInteger(min, max) {
-    // return Math.floor(Math.random());
-    // return Math.floor(Math.random() * 5);
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// console.log(getRandomInteger(10, 20));
-
-// random uppercase letter A - Z, A-65, Z-90
 function getRandomUppercase() {
     return String.fromCharCode(getRandomInteger(65, 91));
 }
+
 function getRandomLowercase() {
     return String.fromCharCode(getRandomInteger(97, 123));
 }
@@ -35,42 +31,39 @@ function getRandomSymbol() {
     return symbols.charAt(index);
 }
 
-// console.log(getRandomUppercase());
-// console.log(getRandomLowercase());
-// console.log(getRandomNumber());
-// console.log(getRandomSymbol());
+generateButtonEl.addEventListener("click", generatePassword);
 
-generateButtonEl.addEventListener("click", function () {
+function generatePassword() {
     password = "";
     let passwordLength = passwordLengthInputEl.value;
 
     const funcArray = [];
     if (uppercaseCheckboxEl.checked) {
-        password = password + getRandomUppercase();
+        password += getRandomUppercase();
         funcArray.push(getRandomUppercase);
     }
     if (lowercaseCheckboxEl.checked) {
-        password = password + getRandomLowercase();
+        password += getRandomLowercase();
         funcArray.push(getRandomLowercase);
     }
     if (numbersCheckboxEl.checked) {
-        password = password + getRandomNumber();
+        password += getRandomNumber();
         funcArray.push(getRandomNumber);
     }
     if (symbolsCheckboxEl.checked) {
-        password = password + getRandomSymbol();
+        password += getRandomSymbol();
         funcArray.push(getRandomSymbol);
     }
 
     for (let i = 0; i < passwordLength - funcArray.length; i++) {
         const index = getRandomInteger(0, funcArray.length);
-        password = password + funcArray[index]();
+        password += funcArray[index]();
     }
-    console.log(funcArray);
 
+    console.log(funcArray);
     console.log(password);
     passwordDisplayEl.value = password;
-});
+}
 
 copyButtonEl.addEventListener("click", copyPasswordToClipboard);
 
@@ -82,3 +75,9 @@ async function copyPasswordToClipboard() {
         console.error("failed to copy password to clipboard", error);
     }
 }
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        generatePassword();
+    }
+});
