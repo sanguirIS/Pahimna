ceadd722f1eddf01bff9cf8233d0f4d5f461a606
DEVKLENN <121370627+sanguirIS@users.noreply.github.com>
2025-10-14 18:35:23 -0700

    commit

--- JavaScript/codis.js
+++ JavaScript/codis.js
@@ -9,7 +9,9 @@ let captchaText = null;
 const generateCaptcha = () => {
   const randomString = Math.random().toString(36).substring(2, 7);
   const randomStringArray = randomString.split("");
-  const changeString = randomStringArray.map((char) => (Math.random() > 0.5 ? char.toUpperCase() : char));
+  const changeString = randomStringArray.map((char) =>
+    Math.random() > 0.5 ? char.toUpperCase() : char
+  );
   captchaText = changeString.join("   ");
   captchaTextBox.value = captchaText;
   console.log(captchaText);
