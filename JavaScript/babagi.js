50f980d021177e020fee15a63a13519e484da3b7
DEVKLENN <121370627+sanguirIS@users.noreply.github.com>
2025-10-14 18:35:23 -0700

    commit

--- JavaScript/babagi.js
+++ JavaScript/babagi.js
@@ -1,16 +1,13 @@
 AOS.init();
 AOS.init({
-  
   offset: 120,
   delay: 0,
   duration: 700,
-  easing: 'ease',
+  easing: "ease",
   once: false,
   mirror: false,
-  anchorPlacement: 'top-bottom',
-
+  anchorPlacement: "top-bottom",
 });
-
 
 const SCROLL_THRESHOLD = 60;
 const IMG_WIDTH = 14;
@@ -18,9 +15,9 @@ const IMG_WIDTH = 14;
 class Slider {
   constructor(element) {
     this.element = element;
-    this.carousel = element.querySelector('.carousel');
-    this.arrowIcons = element.querySelectorAll('.wrapper i');
-    this.firstImg = this.carousel.querySelectorAll('img')[0];
+    this.carousel = element.querySelector(".carousel");
+    this.arrowIcons = element.querySelectorAll(".wrapper i");
+    this.firstImg = this.carousel.querySelectorAll("img")[0];
     this.isDragStart = false;
     this.isDragging = false;
     this.prevPageX = 0;
@@ -30,17 +27,17 @@ class Slider {
   }
 
   init() {
-    this.carousel.addEventListener('mousedown', this.dragStart.bind(this));
-    this.carousel.addEventListener('touchstart', this.dragStart.bind(this));
+    this.carousel.addEventListener("mousedown", this.dragStart.bind(this));
+    this.carousel.addEventListener("touchstart", this.dragStart.bind(this));
 
-    document.addEventListener('mousemove', this.dragging.bind(this));
-    this.carousel.addEventListener('touchmove', this.dragging.bind(this));
+    document.addEventListener("mousemove", this.dragging.bind(this));
+    this.carousel.addEventListener("touchmove", this.dragging.bind(this));
 
-    document.addEventListener('mouseup', this.dragStop.bind(this));
-    this.carousel.addEventListener('touchend', this.dragStop.bind(this));
+    document.addEventListener("mouseup", this.dragStop.bind(this));
+    this.carousel.addEventListener("touchend", this.dragStop.bind(this));
 
-    this.arrowIcons.forEach(icon => {
-      icon.addEventListener('click', this.arrowClick.bind(this));
+    this.arrowIcons.forEach((icon) => {
+      icon.addEventListener("click", this.arrowClick.bind(this));
     });
   }
 
@@ -54,7 +51,7 @@ class Slider {
     if (!this.isDragStart) return;
     e.preventDefault();
     this.isDragging = true;
-    this.carousel.classList.add('dragging');
+    this.carousel.classList.add("dragging");
     this.positionDiff = (e.pageX || e.touches[0].pageX) - this.prevPageX;
     this.carousel.scrollLeft = this.prevScrollLeft - this.positionDiff;
     this.showHideIcons();
@@ -62,7 +59,7 @@ class Slider {
 
   dragStop() {
     this.isDragStart = false;
-    this.carousel.classList.remove('dragging');
+    this.carousel.classList.remove("dragging");
 
     if (!this.isDragging) return;
     this.isDragging = false;
@@ -71,14 +68,17 @@ class Slider {
 
   arrowClick(e) {
     const firstImgWidth = this.firstImg.clientWidth + IMG_WIDTH;
-    this.carousel.scrollLeft += e.target.id == 'left' ? -firstImgWidth : firstImgWidth;
+    this.carousel.scrollLeft +=
+      e.target.id == "left" ? -firstImgWidth : firstImgWidth;
     this.showHideIcons();
   }
-  
+
   showHideIcons() {
     const scrollWidth = this.carousel.scrollWidth - this.carousel.clientWidth;
-    this.arrowIcons[0].style.display = this.carousel.scrollLeft == 0 ? 'none' : 'block';
-    this.arrowIcons[1].style.display = this.carousel.scrollLeft == scrollWidth ? 'none' : 'block';
+    this.arrowIcons[0].style.display =
+      this.carousel.scrollLeft == 0 ? "none" : "block";
+    this.arrowIcons[1].style.display =
+      this.carousel.scrollLeft == scrollWidth ? "none" : "block";
   }
 
   autoSlide() {
@@ -87,12 +87,19 @@ class Slider {
     const firstImgWidth = this.firstImg.clientWidth + IMG_WIDTH;
     const valDifference = firstImgWidth - positionDiff;
 
-    if (this.carousel.scrollLeft - (scrollWidth - this.carousel.clientWidth) > -1 || this.carousel.scrollLeft <= 0) return;
+    if (
+      this.carousel.scrollLeft - (scrollWidth - this.carousel.clientWidth) >
+        -1 ||
+      this.carousel.scrollLeft <= 0
+    )
+      return;
 
     if (this.carousel.scrollLeft > this.prevScrollLeft) {
-      this.carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+      this.carousel.scrollLeft +=
+        positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
     } else {
-      this.carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+      this.carousel.scrollLeft -=
+        positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
     }
   }
 }
@@ -102,15 +109,12 @@ function validateEmail() {
   var gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
   var hotmailRegex = /^[a-zA-Z0-9._%+-]+@hotmail\.com$/;
   if (gmailRegex.test(email) || hotmailRegex.test(email)) {
-
-      return true;
+    return true;
   } else {
-      alert("Please enter a valid Gmail or Hotmail email address.");
-      return false;
+    alert("Please enter a valid Gmail or Hotmail email address.");
+    return false;
   }
 }
 
-
-const memoriesSlider = new Slider(document.querySelector('.memories-slider'));
-const cousinSlider = new Slider(document.querySelector('.cousin-slider'));
-
+const memoriesSlider = new Slider(document.querySelector(".memories-slider"));
+const cousinSlider = new Slider(document.querySelector(".cousin-slider"));
